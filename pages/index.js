fdba25b9f71dfb29d8591361e651621a2cdc14e7
bhok1386 <brandon.hok@gmail.com>
2021-09-07 14:24:11 -0400

    [CSS] - Changed checkbox margins

--- pages/index.js
+++ pages/index.js
@@ -7,94 +7,94 @@ export default function Home() {
     const [checked, setChecked] = useState(false)
 
     return (
-        <div className={styles.checkboxContainer}>
-              <Checkbox
-                checked={checked}
-                onChange={(val) => setChecked(val)}
-                label={"Count me in!"}
-              />
+        // <div className={styles.checkboxContainer}>
+        //       <Checkbox
+        //         checked={checked}
+        //         onChange={(val) => setChecked(val)}
+        //         label={"One World"}
+        //       />
+        //
+        //       <Checkbox
+        //             checked={checked}
+        //             onChange={(val) => setChecked(val)}
+        //             label={"Skyteam"}
+        //       />
+        // </div>
 
-              <Checkbox
-                    checked={checked}
-                    onChange={(val) => setChecked(val)}
-                    label={"Count me in!"}
-              />
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <header className={styles.footer}>
+        <a
+            href="https://www.kayak.com"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+
+          <img src="/kayak.svg" alt="Kayak Logo" className={styles.logo} />
+        </a>
+      </header>
+
+
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Airlines
+        </h1>
+        <h1 className={styles.description}>
+          Filter by Alliances
+        </h1>
+
+        {/*<code className={styles.code}></code>*/}
+
+          <checkboxcontainer className={styles.checkboxContainer} >
+            <input className={styles.checkbox} type={"checkbox"} />
+            <label className={styles.checkboxDescription}> OneWorld</label>
+            <span class={styles.checkbox}></span>
+          {/*  <input className={styles.checkbox} type={"checkbox"} />*/}
+          {/*  <label className={styles.checkboxDescription}> Sky Team</label>*/}
+          {/*  <input className={styles.checkbox} type={"checkbox"} />*/}
+          {/*  <label className={styles.checkboxDescription}> Star Alliance</label>*/}
+          </checkboxcontainer>
+
+
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <img src="/kayak.svg" alt="Kayak Logo" className={styles.avatar} />
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+            <p2>Testing</p2>
+
+          </a>
+
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h3>Learn &rarr;</h3>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+
+          <a
+            href="https://github.com/vercel/next.js/tree/master/examples"
+            className={styles.card}
+          >
+            <h3>Examples &rarr;</h3>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+
+          <a
+            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h3>Deploy &rarr;</h3>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
         </div>
+      </main>
 
-    // <div className={styles.container}>
-    //   <Head>
-    //     <title>Create Next App</title>
-    //     <link rel="icon" href="/favicon.ico" />
-    //   </Head>
-    //
-    //   <header className={styles.footer}>
-    //     <a
-    //         href="https://www.kayak.com"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //     >
-    //
-    //       <img src="/kayak.svg" alt="Kayak Logo" className={styles.logo} />
-    //     </a>
-    //   </header>
-    //
-    //
-    //
-    //   <main className={styles.main}>
-    //     <h1 className={styles.title}>
-    //       Airlines
-    //     </h1>
-    //     <h1 className={styles.description}>
-    //       Filter by Alliances
-    //     </h1>
-    //
-    //     {/*<code className={styles.code}></code>*/}
-    //
-    //       <checkboxcontainer className={styles.checkboxContainer} >
-    //         <input className={styles.checkbox} type={"checkbox"} />
-    //         <label className={styles.checkboxDescription}> OneWorld</label>
-    //         <span class={styles.checkbox}></span>
-    //       {/*  <input className={styles.checkbox} type={"checkbox"} />*/}
-    //       {/*  <label className={styles.checkboxDescription}> Sky Team</label>*/}
-    //       {/*  <input className={styles.checkbox} type={"checkbox"} />*/}
-    //       {/*  <label className={styles.checkboxDescription}> Star Alliance</label>*/}
-    //       </checkboxcontainer>
-    //
-    //
-    //     <div className={styles.grid}>
-    //       <a href="https://nextjs.org/docs" className={styles.card}>
-    //         <img src="/kayak.svg" alt="Kayak Logo" className={styles.avatar} />
-    //         <h3>Documentation &rarr;</h3>
-    //         <p>Find in-depth information about Next.js features and API.</p>
-    //         <p2>Testing</p2>
-    //
-    //       </a>
-    //
-    //       <a href="https://nextjs.org/learn" className={styles.card}>
-    //         <h3>Learn &rarr;</h3>
-    //         <p>Learn about Next.js in an interactive course with quizzes!</p>
-    //       </a>
-    //
-    //       <a
-    //         href="https://github.com/vercel/next.js/tree/master/examples"
-    //         className={styles.card}
-    //       >
-    //         <h3>Examples &rarr;</h3>
-    //         <p>Discover and deploy boilerplate example Next.js projects.</p>
-    //       </a>
-    //
-    //       <a
-    //         href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-    //         className={styles.card}
-    //       >
-    //         <h3>Deploy &rarr;</h3>
-    //         <p>
-    //           Instantly deploy your Next.js site to a public URL with Vercel.
-    //         </p>
-    //       </a>
-    //     </div>
-    //   </main>
-    //
-    // </div>
+    </div>
   )
 }
