92acf1a3ff71899052958d46f00c605cff74f67b
bhok1386 <brandon.hok@gmail.com>
2021-09-07 13:27:24 -0400

    [CSS] - Added multiple checkboxes

--- components/Checkbox.js
+++ components/Checkbox.js
@@ -8,35 +8,52 @@ export default function Checkbox({ checked,onChange,label}) {
 
     function onChange(val){
         setIsChecked(val)
-        console.log("hello")
+        console.log("hello1")
         toggleCheck()
         console.log(isChecked)
     }
 
+    function onChange2(val){
+        setIsChecked2(val)
+        console.log("hello2")
+        toggleCheck2()
+        console.log(isChecked2)
+    }
+
     const [isChecked, setIsChecked] = useState(false);
+    const [isChecked2, setIsChecked2] = useState(false);
 
     const toggleCheck = () => {
         setIsChecked(!isChecked);
     };
 
-    // return (
-    //    <div className="checkbox">
-    //        <div className="border" onClick={() => onChange(!checked)}>
-    //            <div className={`indicator ${checked ? "checked" : ""}`} />
-    //        </div>
-    //             <div className="label">{label}</div>
-    //    </div>
-    // );
+    const toggleCheck2 = () => {
+        setIsChecked2(!isChecked2);
+    };
 
     return (
         <div className={styles.checkbox}>
+
             <div className={styles.border} onClick={() => onChange(!checked)}>
                 {isChecked ? <div className={styles.indicator}></div> : ""}
-                {/*<div className={styles.indicator}></div>*/}
             </div>
 
-        </div>
+            <div className={styles.checkboxLabel}>
+                {label}
+            </div>
 
+            {/*a*/}
+
+            <div className={styles.border} onClick={() => onChange2(!checked)}>
+                {isChecked2 ? <div className={styles.indicator}></div> : ""}
+            </div>
+
+            <div className={styles.checkboxLabel}>
+                {label}
+            </div>
+
+
+        </div>
 
         // <div className={styles.checkbox }>
         //     <div className={styles.border}></div>
