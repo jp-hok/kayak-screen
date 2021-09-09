5edd51a9936f80dd99670a41c6303f9e43917105
bhok1386 <brandon.hok@gmail.com>
2021-09-09 17:24:19 -0400

    [JS] - Strange async solution [temp]

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,141 @@
 import Head from 'next/head'
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import styles from '../styles/Home.module.css'
 import Checkbox from "../components/Checkbox";
 import CardContent from "../components/CardContent";
 import Header from "../components/Header";
 import fetchJsonp from "fetch-jsonp";
 
+let curArray;
+
 export default function Home() {
 
+
+
+    const useAsyncError = () => {
+        const [_, setError] = React.useState();
+        return React.useCallback(
+            e => {
+                setError(() => {
+                    throw e;
+                });
+            },
+            [setError],
+        );
+    };
+
+    let getEntry = async function(){
+
+        console.log('hello inside')
+        // const [data, setData] = useState(null);
+        // const throwError = useAsyncError();
+
+        return await fetchJsonp('https://www.kayak.com/h/mobileapis/directory/airlines/homework.jsonp', {
+            jsonpCallback: 'jsonp',
+        }).then(
+            async function (response){
+                console.log('hello')
+                console.log(response.json())
+                let responseJsonFiltered = await Object.values(response.json()).filter( ( element => element.alliance =="ST"))
+                console.log(responseJsonFiltered)
+                return(responseJsonFiltered)
+            }
+
+        ).catch(function(ex) {
+            console.log('parsing failed', ex)
+        })
+
+    }
 
     // https://www.kayak.com/h/mobileapis/directory/airlines/homework?jsonp=jsonp
   function getP(){
 
-    fetchJsonp('https://www.kayak.com/h/mobileapis/directory/airlines/homework.jsonp', {
+  return fetchJsonp('https://www.kayak.com/h/mobileapis/directory/airlines/homework.jsonp', {
       jsonpCallback: 'jsonp',
     })
         .then(function(response) {
 
             //json.filter
-            let responseJson = response.json()
-            let responseJsonFiltered = Object.values(responseJson).filter( ( element => element.alliance =="ST"))
-
+            // let responseJson = response.json()
+            // let responseJsonFiltered = Object.values(responseJson).filter( ( element => element.alliance =="ST"))
+            // console.log("first" + responseJsonFiltered)
             return response.json()
             //return responseJsonFiltered
-        }).then(function(json) {
+        }
+        ).then(function(json) {
         //console.log('parsed json', json)
-        console.log(json)
-        console.log(json[0])
+        //console.log(json)
+        //console.log(json[0])
         let test = json
 
         //json.filter
         let result = Object.values(json).filter( ( element => element.alliance =="ST"))
-
         console.log(result)
         return result
         ///console.log(test.orders.filter.( element => element.website =="yahoo"))
-    }).catch(function(ex) {
+    }
+    )
+        .catch(function(ex) {
       console.log('parsing failed', ex)
-    })
+    }
+    )
   }
 
-  function testingPOutput(){
-      getP()
-      return <h1>hello out </h1>
+  async function testingPOutput(){
+      // let result = getP()
+      //     .then(function(response) {
+      //   return response
+      // }
+      // )
+      // console.log("------------------------------")
+      // //console.log("-----------" + result )
+      // console.log(state)
+
+      const [data, setData] = React.useState(null);
+      const throwError = useAsyncError();
+
+      let something = await getEntry()
+      console.log(something)
   }
+
+    function FetchOneResource() {
+        const [valueA, setValueA] = useState(null);
+        const [errorA, setErrorA] = useState(null);
+        const [loadingA, setLoadingA] = useState(true);
+        async function getA() {
+            try {
+                setLoadingA(true);
+                console.log('hello------------')
+                const valueA = await getP();
+                setValueA(JSON.stringify(valueA));
+                curArray = valueA
+                //console.log(curArray)
+            } catch (e) {
+                setErrorA(e);
+                console.log(e)
+            } finally {
+                setLoadingA(false);
+            }
+        }
+        useEffect(() => {
+            getA();
+        }, []);
+
+        if (errorA) return "Failed to load resource A";
+        return loadingA ? "Loading..." : valueA;
+    }
+
+
 
     return (
 
         <div>
             hello
-          {getP()}
-
+          {FetchOneResource()}
+            {/*{getEntry()}*/}
           {/*<CardContent/>*/}
-
+            {console.log(curArray)}
+            hello
         </div>
 
     // <div className={styles.container}>
