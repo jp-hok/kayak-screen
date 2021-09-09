a100705c6c4e53191229e0e986bb1e89c132b3aa
bhok1386 <brandon.hok@gmail.com>
2021-09-09 13:28:17 -0400

    [JS] - Added hover for description

--- components/CardContent.js
+++ components/CardContent.js
@@ -1,24 +1,30 @@
 import styles from "../styles/CardContent.module.css";
-import React from "react";
+import React, {useState} from "react";
 
 export default function CardContent({imageSrc,airlineName,allianceName,phoneNum,website}){
 
+    const [isShown, setIsShown] = useState(false)
+
     return(
-        <a href="https://nextjs.org/docs" className={styles.card}>
+        <a href="https://nextjs.org/docs" className={styles.card}
+            onMouseEnter={() => setIsShown(true)}
+            onMouseLeave={() => setIsShown(false)}
+        >
 
             <div className={styles.avatarContainer}>
                 <img src={imageSrc} alt="Kayak Logo" className={styles.avatar} />
             </div>
 
             <div className={styles.contactContainer}>
+
                 <div className={styles.title}>{airlineName}</div>
-                <div className={styles.subTitle}>{allianceName}</div>
-                <div className={styles.phone}>{phoneNum}</div>
-                <div className={styles.web}>{website}</div>
+                {isShown ?  <div className={styles.subTitle}>{allianceName}</div> : ""}
+                {isShown ? <div className={styles.phone}>{phoneNum}</div> : ""}
+                {isShown ? <div className={styles.web}>{website}</div> : ""}
+
             </div>
 
         </a>
-
     )
 
 }
--- pages/index.js
+++ pages/index.js
@@ -18,156 +18,160 @@ export default function Home() {
         .then(function(response) {
           return response.json()
         }).then(function(json) {
-      //console.log('parsed json', json)
+        //console.log('parsed json', json)
         console.log(json)
         console.log(json[0])
+        let test = json
+        // console.log(test.orders.filter.( element => element.website =="yahoo"))
     }).catch(function(ex) {
       console.log('parsing failed', ex)
     })
-
   }
 
     return (
 
-        <div>
-            hello
-          {getP()}
+        // <div>
+        //     hello
+        //   {getP()}
+        //
+        //   <CardContent/>
+        //
+        // </div>
+
+    <div className={styles.container}>
+
+      <Header/>
+
+      <main className={styles.main}>
+
+          <h1 className={styles.title}>
+            Airlines</h1>
+
+          <h1 className={styles.description}>
+            Filter by Alliances</h1>
+
+          <checkboxcontainer className={styles.checkboxContainer} >
+
+            <Checkbox label = "Oneworld"/>
+            <Checkbox label = "Sky Team"/>
+            <Checkbox label = "Star Alliance"/>
+
+          </checkboxcontainer>
+
+        <div className={styles.grid}>
+
+          <CardContent
+            imageSrc={"/airline.svg"}
+            airlineName={"American Airlines"}
+            allianceName={""}
+            website={""}
+            phoneNum={""}
+          >
+          </CardContent>
+
+          <CardContent
+              imageSrc={"/airline.svg"}
+              airlineName={"Delta"}
+              allianceName={""}
+              website={""}
+              phoneNum={""}
+          >
+          </CardContent>
+
+          <CardContent
+              imageSrc={"/airline.svg"}
+              airlineName={"Ukraine International Airlines"}
+              allianceName={""}
+              website={""}
+              phoneNum={""}
+          >
+          </CardContent>
+
+          <CardContent
+              imageSrc={"/airline.svg"}
+              airlineName={"ANA"}
+              allianceName={""}
+              website={""}
+              phoneNum={""}
+          >
+          </CardContent>
+
+          <CardContent
+              imageSrc={"/airline.svg"}
+              airlineName={"Emirates"}
+              allianceName={""}
+              website={""}
+              phoneNum={""}
+          >
+          </CardContent>
+
+          <CardContent
+              imageSrc={"/airline.svg"}
+              airlineName={"Lufthansa"}
+              allianceName={""}
+              website={""}
+              phoneNum={""}
+          >
+          </CardContent>
+
+          <CardContent
+              imageSrc={"/airline.svg"}
+              airlineName={"Singapore Airlines"}
+              allianceName={""}
+              website={""}
+              phoneNum={""}
+          >
+          </CardContent>
+
+          <CardContent
+              imageSrc={"/airline.svg"}
+              airlineName={"Ukraine International Airlines (UIA)"}
+              allianceName={"Star Alliance"}
+              website={"flyuia.com"}
+              phoneNum={"+1 800 876-0114"}
+          >
+          </CardContent>
+
+          <CardContent
+              imageSrc={"/airline.svg"}
+              airlineName={"Finnair"}
+              allianceName={""}
+              website={""}
+              phoneNum={""}
+          >
+          </CardContent>
+
+          <CardContent
+              imageSrc={"/airline.svg"}
+              airlineName={"Alaska Airlines (AS)"}
+              allianceName={"Oneworld"}
+              website={"alaskaair.com"}
+              phoneNum={"+1 800 252 7522"}
+          >
+          </CardContent>
+
+          <CardContent
+              imageSrc={"/airline.svg"}
+              airlineName={"Avianca"}
+              allianceName={""}
+              website={""}
+              phoneNum={""}
+          >
+          </CardContent>
+
+          <CardContent
+              imageSrc={"/airline.svg"}
+              airlineName={"Swiss"}
+              allianceName={""}
+              website={""}
+              phoneNum={""}
+          >
+          </CardContent>
+
         </div>
 
-    // <div className={styles.container}>
-    //
-    //   <Header/>
-    //
-    //   <main className={styles.main}>
-    //
-    //       <h1 className={styles.title}>
-    //         Airlines</h1>
-    //
-    //       <h1 className={styles.description}>
-    //         Filter by Alliances</h1>
-    //
-    //       <checkboxcontainer className={styles.checkboxContainer} >
-    //
-    //         <Checkbox label = "Oneworld"/>
-    //         <Checkbox label = "Sky Team"/>
-    //         <Checkbox label = "Star Alliance"/>
-    //
-    //       </checkboxcontainer>
-    //
-    //     <div className={styles.grid}>
-    //
-    //       <CardContent
-    //         imageSrc={"/airline.svg"}
-    //         airlineName={"American Airlines"}
-    //         allianceName={""}
-    //         website={""}
-    //         phoneNum={""}
-    //       >
-    //       </CardContent>
-    //
-    //       <CardContent
-    //           imageSrc={"/airline.svg"}
-    //           airlineName={"Delta"}
-    //           allianceName={""}
-    //           website={""}
-    //           phoneNum={""}
-    //       >
-    //       </CardContent>
-    //
-    //       <CardContent
-    //           imageSrc={"/airline.svg"}
-    //           airlineName={"Ukraine International Airlines"}
-    //           allianceName={""}
-    //           website={""}
-    //           phoneNum={""}
-    //       >
-    //       </CardContent>
-    //
-    //       <CardContent
-    //           imageSrc={"/airline.svg"}
-    //           airlineName={"ANA"}
-    //           allianceName={""}
-    //           website={""}
-    //           phoneNum={""}
-    //       >
-    //       </CardContent>
-    //
-    //       <CardContent
-    //           imageSrc={"/airline.svg"}
-    //           airlineName={"Emirates"}
-    //           allianceName={""}
-    //           website={""}
-    //           phoneNum={""}
-    //       >
-    //       </CardContent>
-    //
-    //       <CardContent
-    //           imageSrc={"/airline.svg"}
-    //           airlineName={"Lufthansa"}
-    //           allianceName={""}
-    //           website={""}
-    //           phoneNum={""}
-    //       >
-    //       </CardContent>
-    //
-    //       <CardContent
-    //           imageSrc={"/airline.svg"}
-    //           airlineName={"Singapore Airlines"}
-    //           allianceName={""}
-    //           website={""}
-    //           phoneNum={""}
-    //       >
-    //       </CardContent>
-    //
-    //       <CardContent
-    //           imageSrc={"/airline.svg"}
-    //           airlineName={"Ukraine International Airlines (UIA)"}
-    //           allianceName={"Star Alliance"}
-    //           website={"flyuia.com"}
-    //           phoneNum={"+1 800 876-0114"}
-    //       >
-    //       </CardContent>
-    //
-    //       <CardContent
-    //           imageSrc={"/airline.svg"}
-    //           airlineName={"Finnair"}
-    //           allianceName={""}
-    //           website={""}
-    //           phoneNum={""}
-    //       >
-    //       </CardContent>
-    //
-    //       <CardContent
-    //           imageSrc={"/airline.svg"}
-    //           airlineName={"Alaska Airlines (AS)"}
-    //           allianceName={"Oneworld"}
-    //           website={"alaskaair.com"}
-    //           phoneNum={"+1 800 252 7522"}
-    //       >
-    //       </CardContent>
-    //
-    //       <CardContent
-    //           imageSrc={"/airline.svg"}
-    //           airlineName={"Avianca"}
-    //           allianceName={""}
-    //           website={""}
-    //           phoneNum={""}
-    //       >
-    //       </CardContent>
-    //
-    //       <CardContent
-    //           imageSrc={"/airline.svg"}
-    //           airlineName={"Swiss"}
-    //           allianceName={""}
-    //           website={""}
-    //           phoneNum={""}
-    //       >
-    //       </CardContent>
-    //
-    //     </div>
-    //
-    //   </main>
-    //
-    // </div>
+      </main>
+
+    </div>
   )
 }
